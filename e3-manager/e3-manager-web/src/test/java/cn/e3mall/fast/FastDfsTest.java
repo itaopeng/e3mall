9677bc21d74747802b13a5979dd9c0757d64eca3
itaopeng <1094177531@qq.com>
2018-12-16 22:42:19 +0800

    1

---
+++ e3-manager/e3-manager-web/src/test/java/cn/e3mall/fast/FastDfsTest.java
@@ -0,0 +1,31 @@
+package cn.e3mall.fast;
+
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.StorageServer;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
+import org.junit.Test;
+
+public class FastDfsTest {
+	
+		@Test
+		public void testUpload() throws Exception{
+			//创建一个配置文件 文件名任意 内容就是tracker服务器地址
+			//使用全局对象加载配置文件
+			ClientGlobal.init("D:/java/github/e3-manager/e3-manager-web/src/main/resources/conf/client.conf");
+			//创建一个 TrackerClient对象
+			TrackerClient trackerClient = new TrackerClient();
+			//通过TrackerClient获得trackerService对象
+			TrackerServer trackerServer = trackerClient.getConnection();
+			//创建StorageService引用,可以是null
+			StorageServer storageServer = null;
+			//创建一个 StorageClient，参数需要TrackerServer和StorageServer
+			StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+			//使用storageClient上传文件
+			String[] strings = storageClient.upload_file("F:/jj/logo.jpg", "jpg", null);
+			for (String string : strings) {
+				System.out.println(string);
+			}
+		}
+}
