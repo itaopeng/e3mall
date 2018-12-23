0f4e932376991106b45dac8b6a1e80ca97fa9412
itaopeng <1094177531@qq.com>
2018-12-23 14:16:12 +0800

    1

--- e3-manager/e3-manager-web/src/main/java/cn/e3mall/controller/PictureController.java
+++ e3-manager/e3-manager-web/src/main/java/cn/e3mall/controller/PictureController.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import cn.e3mall.common.utils.FastDFSClient;
+import cn.e3mall.common.utils.JsonUtils;
 
 /***
  * 图片上传Controller
@@ -22,9 +24,9 @@ public class PictureController {
 	@Value("${IMAGE_SERVER_URL}")
 	private String IMAGE_SERVER_URL;
 	
-	@RequestMapping("/pic/upload")
+	@RequestMapping(value="/pic/upload",produces=MediaType.TEXT_PLAIN_VALUE+";charset=utf-8")
 	@ResponseBody
-	public Map uploadFile(MultipartFile uploadFile){
+	public String uploadFile(MultipartFile uploadFile){
 		Map result = new HashMap<>();
 		try{
 			//把图片上传到图片服务器
@@ -43,6 +45,6 @@ public class PictureController {
 			result.put("error", 1);
 			result.put("message", "图片上传失败");	 
 		}
-		return result;
+		return JsonUtils.objectToJson(result);
 	}
 }
