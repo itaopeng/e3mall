141049d93715485670110a8403aa9eafddd29205
Administrator <Administrator@tp>
2019-12-22 01:35:53 +0800

    1

--- e3-search/e3-search-service/src/main/java/cn/e3mall/search/dao/SearchDao.java
+++ e3-search/e3-search-service/src/main/java/cn/e3mall/search/dao/SearchDao.java
@@ -15,61 +15,60 @@ import org.springframework.stereotype.Repository;
 import cn.e3mall.common.pojo.SearchItem;
 import cn.e3mall.common.pojo.SearchResult;
 
-/***
+/**
  * 商品搜索dao
  * <p>Title: SearchDao</p>
  * <p>Description: </p>
  * <p>Company: www.itcast.cn</p> 
  * @version 1.0
  */
-
 @Repository
 public class SearchDao {
 	
 	@Autowired
 	private SolrServer solrServer;
-	
-	/***
-	 * 根据查询条件查询索引
+
+	/**
+	 *根据查询条件查询索引库
 	 * <p>Title: search</p>
 	 * <p>Description: </p>
-	 * @param solrQuery
+	 * @param query
 	 * @return
 	 */
 	public SearchResult search(SolrQuery query) throws Exception {
-		//根据query 查询索引库
+		//根据query查询索引库
 		QueryResponse queryResponse = solrServer.query(query);
-		//取查询结果
+		//取查询结果。
 		SolrDocumentList solrDocumentList = queryResponse.getResults();
-		//取查询总记录数
+		//取查询结果总记录数
 		long numFound = solrDocumentList.getNumFound();
-		SearchResult result  = new SearchResult();
+		SearchResult result = new SearchResult();
 		result.setRecordCount(numFound);
-		List<SearchItem> searchItemList = new ArrayList<SearchItem>();
-		
-		//取商品列表 需要高亮显示
+		//取商品列表，需要取高亮显示
 		Map<String, Map<String, List<String>>> highlighting = queryResponse.getHighlighting();
+		List<SearchItem> itemList = new ArrayList<>();
 		for (SolrDocument solrDocument : solrDocumentList) {
 			SearchItem item = new SearchItem();
-			item.setCategory_name(solrDocument.get("item_category_name").toString());
-			item.setId((String) solrDocument.get(solrDocument.get("id")));
-			item.setImage(solrDocument.get("item_image").toString());
+			item.setId((String) solrDocument.get("id"));
+			item.setCategory_name((String) solrDocument.get("item_category_name"));
+			item.setImage((String) solrDocument.get("item_image"));
 			item.setPrice((long) solrDocument.get("item_price"));
-			item.setTitle(solrDocument.get("item_title").toString());
-			item.setSell_point(solrDocument.get("item_sell_point").toString());
-			//添加到商品列表
-			searchItemList.add(item);
+			item.setSell_point((String) solrDocument.get("item_sell_point"));
+			//取高亮显示
 			List<String> list = highlighting.get(solrDocument.get("id")).get("item_title");
 			String title = "";
-			if(list!=null && list.size()>0){
+			if (list != null && list.size() > 0) {
 				title = list.get(0);
-			}else{
+			} else {
 				title = (String) solrDocument.get("item_title");
 			}
 			item.setTitle(title);
+			//添加到商品列表
+			itemList.add(item);
 		}
-		result.setItemList(searchItemList);
+		result.setItemList(itemList);
 		//返回结果
 		return result;
 	}
+	
 }
